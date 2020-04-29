cde0dfade9d2b2d9135c76b216e4feda3a12b5c4
ykurata <yasukokurata@gmail.com>
2020-04-29 16:35:05 -0400

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import market from '../images/market.jpg';
+
+const myStyle = {
+  backgroundImage : `url(${market})` ,
+  backgroundSize: 'cover',
+  height: '100vh',
+  position: 'relative',
+  width: '100%',
+	display: 'table',
+	objectFit: 'cover',
+	opacity: '0.6'
+}
+
+const Home = () => {
+  return (
+    <div id="home" style={myStyle}>
+      <div className="landing-text">
+        <h1>white rabbit.to</h1>
+        <h3>White rabbit is a group of hospitality professinal thats thinks outside of the box, <br></br> 
+					to generate new income streams.
+				</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">White Rabbit.to</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#home">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#skills">Skills</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#projects">Projects</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
